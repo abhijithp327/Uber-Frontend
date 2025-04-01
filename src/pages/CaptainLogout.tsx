289986eb1a131d270718f9a89f4d6e0ec5106848
abhijithp327 <87142203+abhijithp327@users.noreply.github.com>
2025-04-01 18:34:12 +0530

    Added captain logout

--- src/pages/CaptainLogout.tsx
+++ src/pages/CaptainLogout.tsx
@@ -2,31 +2,31 @@ import React from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
-const UserLogout = () => {
+const CaptainLogout = () => {
+
 
     const baseUrl = import.meta.env.VITE_BASE_URL;
     const navigate = useNavigate();
-    
+
+
     React.useEffect(() => {
         const handleLogout = async () => {
-            const response = await axios.post(`${baseUrl}/user/logout`, {}, {
+            const response = await axios.post(`${baseUrl}/captain/logout`, {}, {
                 withCredentials: true
             });
             // console.log('response: ', response);
             if (response.status === 200) {
-                localStorage.removeItem('token');
-                navigate('/login');
+                navigate('/captain-login');
             }
         };
         handleLogout();
     }, []);
 
-
     return (
         <div>
-            UserLogout
+            captain logout
         </div>
-    );
-};
+    )
+}
 
-export default UserLogout;
+export default CaptainLogout
