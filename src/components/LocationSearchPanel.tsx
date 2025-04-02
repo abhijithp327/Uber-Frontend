c569cde61992d0fd4b29454178624e4c09ca429e
abhijithp327 <87142203+abhijithp327@users.noreply.github.com>
2025-04-02 18:06:13 +0530

    Added vehicles

--- src/components/LocationSearchPanel.tsx
+++ src/components/LocationSearchPanel.tsx
@@ -3,7 +3,37 @@ import React from 'react';
 const LocationSearchPanel = () => {
     return (
         <div>
-            
+            <div className='flex flex-col gap-4'>
+
+                <div className='flex items-center gap-3'>
+                    <div className='bg-[#eee] w-8 h-7 flex items-center justify-center rounded-full'>
+                        <i className='ri-map-pin-fill text-md text-black'></i>
+                    </div>
+                    <h4 className='text-sm font-medium'>Nooranad near swathy theatre, sudhi bakery 690504</h4>
+                </div>
+
+                <div className='flex items-center gap-3'>
+                    <div className='bg-[#eee] w-8 h-7 flex items-center justify-center rounded-full'>
+                        <i className='ri-map-pin-fill text-md text-black'></i>
+                    </div>
+                    <h4 className='text-sm font-medium'>Nooranad near swathy theatre, sudhi bakery 690504</h4>
+                </div>
+
+                <div className='flex items-center gap-3'>
+                    <div className='bg-[#eee] w-8 h-7 flex items-center justify-center rounded-full'>
+                        <i className='ri-map-pin-fill text-md text-black'></i>
+                    </div>
+                    <h4 className='text-sm font-medium'>Nooranad near swathy theatre, sudhi bakery 690504</h4>
+                </div>
+
+                <div className='flex items-center gap-3'>
+                    <div className='bg-[#eee] w-8 h-7 flex items-center justify-center rounded-full'>
+                        <i className='ri-map-pin-fill text-md text-black'></i>
+                    </div>
+                    <h4 className='text-sm font-medium'>Nooranad near swathy theatre, sudhi bakery 690504</h4>
+                </div>
+
+            </div>
         </div>
     );
 };
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -81,6 +81,119 @@ const Home = () => {
                 </div>
             </div>
 
+            <div className='fixed w-full z-10 bottom-0 p-5 bg-white flex flex-col gap-4'>
+
+                <div className='bg-white border flex items-center justify-between p-2 w-full rounded-lg hover:border-black hover:cursor-pointer hover:border-2'>
+
+                    <img className='h-14' src="https://swyft.pl/wp-content/uploads/2023/05/can-1-person-use-uberx.jpg" alt="car" />
+
+                    <div className='flex flex-col flex-grow ml-5'>
+
+                        {/* UberGo, User Count & Price in Same Row */}
+                        <div className='flex items-center justify-between w-full'>
+                            <div className='flex items-center gap-2'>
+                                <h4 className='text-sm font-medium'>UberGo</h4>
+                                <div className='flex items-center gap-1'>
+                                    <i className='ri-user-3-fill text-base'></i>
+                                    <span className='text-sm'>4</span>
+                                </div>
+                            </div>
+
+                            {/* Price moved inside this row */}
+                            <h2 className='text-base font-medium'>₹150.30</h2>
+                        </div>
+
+                        <h5 className='text-sm font-medium'>2 mins away</h5>
+                        <p className='text-xs font-normal text-gray-700'>Affordable, compact rides</p>
+
+                    </div>
+
+                </div>
+
+                <div className='bg-white border flex items-center justify-between p-2 w-full rounded-lg hover:border-black hover:cursor-pointer hover:border-2'>
+
+                    <img className='h-16' src="https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_368,w_552/v1649231091/assets/2c/7fa194-c954-49b2-9c6d-a3b8601370f5/original/Uber_Moto_Orange_312x208_pixels_Mobile.png" alt="car" />
+
+                    <div className='flex flex-col flex-grow ml-5'>
+
+                        {/* UberGo, User Count & Price in Same Row */}
+                        <div className='flex items-center justify-between w-full'>
+                            <div className='flex items-center gap-2'>
+                                <h4 className='text-sm font-medium'>Moto</h4>
+                                <div className='flex items-center gap-1'>
+                                    <i className='ri-user-3-fill text-base'></i>
+                                    <span className='text-sm'>1</span>
+                                </div>
+                            </div>
+
+                            {/* Price moved inside this row */}
+                            <h2 className='text-base font-medium'>₹65.17</h2>
+                        </div>
+
+                        <h5 className='text-sm font-medium'>3 mins away</h5>
+                        <p className='text-xs font-normal text-gray-700'>Affordable, motorcyle rides</p>
+
+                    </div>
+
+                </div>
+
+                <div className='bg-white border flex items-center justify-between p-2 w-full rounded-lg hover:border-black hover:cursor-pointer hover:border-2'>
+
+                    <img className='h-16' src="https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_368,w_552/v1682350114/assets/c2/296eac-574a-4a81-a787-8a0387970755/original/UberBlackXL.png" alt="car" />
+
+                    <div className='flex flex-col flex-grow ml-5'>
+
+                        {/* UberGo, User Count & Price in Same Row */}
+                        <div className='flex items-center justify-between w-full'>
+                            <div className='flex items-center gap-2'>
+                                <h4 className='text-sm font-medium'>Premier</h4>
+                                <div className='flex items-center gap-1'>
+                                    <i className='ri-user-3-fill text-base'></i>
+                                    <span className='text-sm'>5</span>
+                                </div>
+                            </div>
+
+                            {/* Price moved inside this row */}
+                            <h2 className='text-base font-medium'>₹700.17</h2>
+                        </div>
+
+                        <h5 className='text-sm font-medium'>5 mins away</h5>
+                        <p className='text-xs font-normal text-gray-700'>Quality drivers</p>
+
+                    </div>
+
+                </div>
+
+                <div className='bg-white border flex items-center justify-between p-2 w-full rounded-lg hover:border-black hover:cursor-pointer hover:border-2'>
+
+                    <img className='h-16' src="https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_368,w_552/v1648431773/assets/1d/db8c56-0204-4ce4-81ce-56a11a07fe98/original/Uber_Auto_558x372_pixels_Desktop.png" alt="car" />
+
+                    <div className='flex flex-col flex-grow ml-5'>
+
+                        {/* UberGo, User Count & Price in Same Row */}
+                        <div className='flex items-center justify-between w-full'>
+                            <div className='flex items-center gap-2'>
+                                <h4 className='text-sm font-medium'>Auto</h4>
+                                <div className='flex items-center gap-1'>
+                                    <i className='ri-user-3-fill text-base'></i>
+                                    <span className='text-sm'>3</span>
+                                </div>
+                            </div>
+
+                            {/* Price moved inside this row */}
+                            <h2 className='text-base font-medium'>₹100.17</h2>
+                        </div>
+
+                        <h5 className='text-sm font-medium'>5 mins away</h5>
+                        <p className='text-xs font-normal text-gray-700'>Quality drivers</p>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+
         </div>
     );
 };
